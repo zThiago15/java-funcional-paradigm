16969603c0ee31e5e49f5907ffad1d4aa00dfde2
zThiago15 <thiaguinhodias.15@gmail.com>
2021-07-25 13:10:08 -0300

    Testing concepts of funcional and imperative paradigm, declaration and immutability

---
+++ src/main/java/one/innovation/digital/Imutabilidade.java
@@ -0,0 +1,14 @@
+package one.innovation.digital;
+
+import java.util.function.UnaryOperator;
+
+public class Imutabilidade {
+
+    public static void main(String[] args) {
+
+        int valor = 40;
+        UnaryOperator<Integer> retornaDobro = v -> v * 2;
+        System.out.println(retornaDobro.apply(valor));
+        System.out.println(valor); //value of the variable not changed
+    }
+}
