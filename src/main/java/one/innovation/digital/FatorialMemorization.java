83963db3846da618c677859d979c9e93b45cad79
zThiago15 <thiaguinhodias.15@gmail.com>
2021-07-25 21:59:51 -0300

    Testando conceitos de paradigma funcional, imperativo e recursividade

---
+++ src/main/java/one/innovation/digital/FatorialMemorization.java
@@ -0,0 +1,33 @@
+package one.innovation.digital;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FatorialMemorization {
+
+    static Map<Integer, Integer> MAPA_FATORIAL = new HashMap<>();
+    public static void main(String[] args) {
+        long T = System.nanoTime();
+        System.out.println(fatorialComMemorization(15));
+        long F = System.nanoTime();
+        System.out.println("Fatorial 1 "+ (F-T));
+
+        T = System.nanoTime();
+        System.out.println(fatorialComMemorization(15));
+        F = System.nanoTime();
+        System.out.println("Fatorial 2 " + (F-T));
+    }
+
+    public static Integer fatorialComMemorization(Integer value) {
+        if (value == 1) {
+            return value;
+        } else {
+            if (MAPA_FATORIAL.containsKey(value)){
+                return MAPA_FATORIAL.get(value);
+            } else {
+                Integer resultado = value * fatorialComMemorization(value -1);
+                return resultado;
+            }
+        }
+    }
+}
---
+++ src/main/java/one/innovation/digital/fatorialRecursivo/FatorialTailCall.java
@@ -0,0 +1,21 @@
+package one.innovation.digital.fatorialRecursivo;
+
+public class FatorialTailCall {
+
+    public static void main(String[] args) {
+        System.out.println(fatorialA(5));
+    }
+
+    public static double fatorialA(double valor){
+        return fatorialComTailCall(valor, 1);
+    }
+
+    public static double fatorialComTailCall(double valor, double numero){
+        if(valor == 0){
+            return numero;
+        }
+        return fatorialComTailCall(valor-1, numero*valor);
+    }
+    //É mais rápido que a recursividade padrão
+}
+
